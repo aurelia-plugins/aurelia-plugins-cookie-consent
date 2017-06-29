a914d169fe336a715830b59d42ba1b9d6887afd9
Bart Lenaerts <bart.lenaerts@philips.com>
2017-06-29 11:40:04 +0200

    Typescript definitions

---
+++ dist/system/aurelia-plugins-cookie-consent-element.d.ts
@@ -0,0 +1,9 @@
+export declare class CookieConsent {
+    _config: any;
+    _element: any;
+    button: string;
+    message: string;
+    show: boolean;
+    constructor(element: any, config: any);
+    dismiss(): void;
+}
